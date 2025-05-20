65f193c53b4e4168ef715abe9dc6daae938458cf
Toastvillein <okkcba7899@gmail.com>
2025-05-20 21:26:59 +0900

    feat: delete cart items

--- src/main/java/com/example/eightflix/domain/food/controller/CartItemController.java
+++ src/main/java/com/example/eightflix/domain/food/controller/CartItemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +41,13 @@ public class CartItemController {
 		List<ItemResponse> allItems = cartItemService.findAllItems(cartId);
 
 		return ResponseEntity.status(HttpStatus.OK).body(allItems);
+	}
+
+	@DeleteMapping("/{cartItemId}")
+	public ResponseEntity<Void> deleteItems(@PathVariable Long cartItemId){
+
+		cartItemService.deleteItems(cartItemId);
+
+		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 }
