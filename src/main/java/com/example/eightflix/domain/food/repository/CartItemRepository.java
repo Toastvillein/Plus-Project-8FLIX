e80ca21ef5120275c544b752c746af93faacfc19
Toastvillein <okkcba7899@gmail.com>
2025-05-20 20:39:11 +0900

    feat: find all cartItems

--- src/main/java/com/example/eightflix/domain/food/repository/CartItemRepository.java
+++ src/main/java/com/example/eightflix/domain/food/repository/CartItemRepository.java
@@ -3,6 +3,8 @@ package com.example.eightflix.domain.food.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.eightflix.domain.food.entity.Cart;
@@ -11,5 +13,10 @@ import com.example.eightflix.domain.food.entity.Food;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
-	Optional<CartItem> findByCartAndFood(Cart cart, Food food);
+	@Query("SELECT i FROM CartItem i JOIN FETCH i.food JOIN FETCH i.cart "
+		+ "WHERE i.cart =:cart AND i.food = :food")
+	Optional<CartItem> findByCartAndFood(@Param("cart") Cart cart,@Param("food") Food food);
+
+	@Query("SELECT i FROM CartItem i JOIN FETCH i.food WHERE i.cart = :cart")
+	Optional<CartItem> findAllByCart(@Param("cart") Cart Cart);
 }
--- src/main/java/com/example/eightflix/global/config/SecurityConfig.java
+++ src/main/java/com/example/eightflix/global/config/SecurityConfig.java
@@ -24,5 +24,4 @@ public class SecurityConfig {
                         .anyRequest().authenticated());
 		return http.build();
 	}
-
 }
