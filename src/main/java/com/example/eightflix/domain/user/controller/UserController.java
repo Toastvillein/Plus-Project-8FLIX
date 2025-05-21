6cac231f50ba785f95cf112f049b0dd243b4842d
yeonjookang <96986782+yeonjookang@users.noreply.github.com>
2025-05-21 18:05:51 +0900

    feat: jwtfilter을 이용한 인증/인가 구현

--- src/main/java/com/example/eightflix/domain/user/controller/UserController.java
+++ src/main/java/com/example/eightflix/domain/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.example.eightflix.domain.user.controller;
 
 
+import com.example.eightflix.domain.user.dto.GetUserResponse;
 import com.example.eightflix.domain.user.dto.SignUpRequest;
 import com.example.eightflix.domain.user.service.UserService;
+import com.example.eightflix.global.security.CurrentUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/api/signup")
+    @PostMapping("/api/auth/signup")
     public ResponseEntity<Void> signup(@Valid @RequestBody SignUpRequest signUpRequest) {
-        return userService.signUp(signUpRequest);
+        userService.signUp(signUpRequest);
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/api/users")
+    public ResponseEntity<GetUserResponse> getUser(@CurrentUser Long id) {
+        return ResponseEntity.ok(userService.getUser(id));
     }
 }
---
+++ src/main/java/com/example/eightflix/global/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.eightflix.global.security;
+
+import com.example.eightflix.domain.user.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final JwtFilter jwtFilter;
+	private final CustomAccessDeniedHandler accessDeniedHandler;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		http
+				.csrf(csrf -> csrf.disable())
+				.exceptionHandling(ex -> ex
+						.accessDeniedHandler(accessDeniedHandler)
+				)
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/api/auth/**").permitAll()
+						.requestMatchers("/api/admin/**").hasRole("ADMIN")
+						.anyRequest().authenticated()
+				)
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+}
